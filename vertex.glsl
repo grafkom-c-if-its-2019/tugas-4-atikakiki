799115f36aa527d6d86a7613f954bcfca3c6bb7b
atikakiki <atikarizki41@gmail.com>
2019-12-08 10:19:41 +0700

    tugas 4

--- vertex.glsl
+++ vertex.glsl
@@ -1,51 +1,52 @@
 precision mediump float;
-#define pi 3.14
-attribute vec2 vPosition;
+
+attribute vec3 vPosition;
 attribute vec3 vColor;
+attribute vec3 vNormal;
+
 varying vec3 fColor;
-uniform vec3 sudut;
+varying vec3 fPosition;
+varying vec3 fNormal;
+
 uniform mat4 projection;
 uniform mat4 view;
+uniform mat4 model;
+
+uniform mat3 normalMatrix;
+
 uniform float scale;
-uniform vec3 mantul;
 uniform float tengah;
+uniform vec3 bounce;
 
 void main() {
-  fColor = vColor;
-  mat4 translate = mat4(
+  mat4 to_origin = mat4(
     1.0, 0.0, 0.0, 0.0,
     0.0, 1.0, 0.0, 0.0,
     0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, -2.0, 1.0         // Kita geser setiap verteks sejau 2 unit menjauhi kamera, untuk memastikan seluruh bagian kubus ada di antara near dan far.
+    0.0, 0.0, -2.0, 1.0 
   );
 
-  mat4 mantul = mat4(
-    1.0, 0.0, 0.0, mantul.x,
-    0.0, 1.0, 0.0, mantul.y,
-    0.0, 0.0, 1.0, mantul.z,
-    0.0, 0.0, 0.0, 1.0        
+  mat4 bounce = mat4(
+    1.0, 0.0, 0.0, bounce.x,
+    0.0, 1.0, 0.0, bounce.y,
+    0.0, 0.0, 1.0, bounce.z,
+    0.0, 0.0, 0.0, 1.0
   );
 
-  // p' = p
-  // p' = T * p
-  //float sudut=0.5*pi;
-  // mat4 rotasi = mat4(
-  //   cos(sudut), -sin(sudut), 0.0, 0.0,
-  //   sin(sudut), cos(sudut), 0.0, 0.0,
-  //   0.0, 0.0, 1.0, 0.0,
-  //   0.0, 0.0, 0.0, 1.0
-  // );
-  // gl_Position = gl_Position * rotasi;
-
-  
-  vec4 tengah_vektor = vec4(tengah, 0, 0, 1.0);
+  vec4 vectengah = vec4(tengah,0,0,1.0);
 
   mat4 skalasi = mat4(
-    scale, 0.0, 0.0, -(tengah_vektor.x)*scale + (tengah_vektor.x),
+    scale, 0.0, 0.0, -(vectengah.x)*scale + (vectengah.x),
     0.0, 1.0, 0.0, 0.0,
     0.0, 0.0, 1.0, 0.0,
     0.0, 0.0, 0.0, 1.0
   );
-  gl_Position = vec4(vPosition, 0.0, 1.0) * mantul * skalasi; //wajib diisi
-  gl_Position = projection * view * translate *  gl_Position ;
+
+  // gl_Position = vec4(vPosition, 1.0) * bounce * skalasi;
+  gl_Position = projection * view * model * vec4(vPosition, 1.0);
+  // urutan perkaliannya harus = projection x view x model (transformasi)
+
+  fColor = vColor;
+  fPosition = vec3(view * model * vec4(vPosition, 1.0));
+  fNormal = normalMatrix * vNormal;
 }
